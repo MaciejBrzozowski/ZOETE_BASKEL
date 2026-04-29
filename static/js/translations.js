4b5e1646eded51ab640b30f746e05d86a88d1d2b
Maciej Brzozowski <maciej.brzozowski.83@gmail.com>
2026-04-29 13:14:37 +0200

    Add fetching text from config file

---
+++ static/js/translations.js
@@ -0,0 +1,61 @@
+let translations = {}
+let href = {}
+let defaultLanguage = 'nl'
+;(function () {
+  axios
+    .get(
+      'https://raw.githubusercontent.com/ZoeteBaksel/-ZOETE_BAKSEL_CONFIG-/refs/heads/master/language.json'
+    )
+    .then(function (response) {
+      if (response.status == 200) {
+        const data = response.data
+        defaultLanguage = data.default
+        translations = data.translations[defaultLanguage]
+        href = data.href
+
+        console.log(data)
+        href.forEach(element => {
+          console.log(element)
+        })
+
+        if (document.documentElement.id == 'index') {
+          const htmlIdList = [
+            'WelcomeText',
+            'DescriptionText',
+            'OfferText',
+            'ClosingText'
+          ]
+          htmlIdList.forEach(htmlId => {
+            document.getElementById(htmlId).innerText = translations[htmlId]
+          })
+        } else if (document.documentElement.id == 'aboutus') {
+          const htmlIdList = [
+            'AboutUsTextWelcome',
+            'AboutUsText',
+            'AboutUsText2',
+            'AboutUsText3'
+          ]
+          htmlIdList.forEach(htmlId => {
+            document.getElementById(htmlId).innerText = translations[htmlId]
+          })
+        } else if (document.documentElement.id == 'contact') {
+          const htmlIdList = [
+            'ContactUsText1',
+            'ContactUsListItem1',
+            'ContactUsListItem2',
+            'ContactUsListItem3',
+            'ContactUsListItem4',
+            'ContactUsText2',
+            'ContactUsText3P1',
+            'ContactUsText3P2',
+            'ContactUsText4',
+            'ContactUsPhoneNumber',
+            'ContactUsEmail'
+          ]
+          htmlIdList.forEach(htmlId => {
+            document.getElementById(htmlId).innerText = translations[htmlId]
+          })
+        }
+      }
+    })
+})()
